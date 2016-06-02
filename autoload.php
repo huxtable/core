253f54ff88c4f1bd041743bf5c23da51fa5e23c7
Ashur Cabrera <hello@cabreramade.co>
2016-06-02 07:53:34 -0700

    adding autoload convenience file

---
+++ autoload.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * This file is part of Huxtable\Core
+ */
+namespace Huxtable\Core;
+
+$pathBaseCore	= __DIR__;
+$pathSrcCore	= $pathBaseCore . '/src/Core';
+$pathVendorCore	= $pathBaseCore . '/vendor';
+
+/*
+ * Initialize autoloading
+ */
+include_once( $pathSrcCore . '/Autoloader.php' );
+Autoloader::register();
